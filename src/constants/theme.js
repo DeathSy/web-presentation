a611c524c7246b0d50fe3f0d0934858adf8d36a9
ksotis <ks.srikittichai@gmail.com>
2020-09-23 12:05:06 +0700

    add presentation theme

---
+++ src/constants/theme.js
@@ -0,0 +1,4 @@
+export const Theme = {
+  BLANKSPACE_BACKGROUND_COLOR: "#000",
+  BACKGROUND_COLOR: "#aaa"
+};
---
+++ src/context/PresentationTheme.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext } from "react";
+import { Theme } from "../constants";
+
+export const PresentationThemeContext = createContext({});
+
+function PresentationThemeProvider({
+  children,
+  backgroundColor = Theme.BACKGROUND_COLOR,
+}) {
+  return (
+    <PresentationThemeContext.Provider
+      value={{
+        backgroundColor,
+        blankspaceBackgroundColor: Theme.BLANKSPACE_BACKGROUND_COLOR,
+      }}
+    >
+      {children}
+    </PresentationThemeContext.Provider>
+  );
+}
+
+export function usePresentationTheme(callback = (theme) => theme) {
+  const presentationTheme = useContext(PresentationThemeContext);
+
+  return callback(presentationTheme);
+}
+
+export default PresentationThemeProvider;
