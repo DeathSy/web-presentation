fff1fb787a39afd56f54a96981e5e23e810d870c
ksotis <ks.srikittichai@gmail.com>
2020-09-23 12:05:22 +0700

    add title page

---
+++ src/pages/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Presentation from "../components/Presentation";
+
+function Title() {
+  return (
+    <Presentation>
+      <div>Title Page</div>
+    </Presentation>
+  );
+}
+
+export default Title;
