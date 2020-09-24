fb6189873497632fb71a9c2b5f03e36e15505df4
ksotis <ks.srikittichai@gmail.com>
2020-09-24 21:03:16 +0700

    change base font color

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,6 +1,6 @@
 export const Theme = {
   FONT: "https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap",
-  FONT_COLOR: "#fff",
+  FONT_COLOR: "#F9F9F9",
   BLANKSPACE_BACKGROUND_COLOR: "#000",
   BACKGROUND_COLOR: "#26282E",
   ASPECT_RATIO: 4 / 3,
