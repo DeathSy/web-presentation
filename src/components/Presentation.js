09576b11943b0a0520e8e9754384a3c42350c117
ksotis <ks.srikittichai@gmail.com>
2020-09-24 13:28:22 +0700

    add configurable presentation template

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -13,16 +13,39 @@ const PresentationContainer = styled.div`
   background-color: ${(props) => props.theme.backgroundColor};
   display: flex;
   width: 100%;
-  height: ${(props) => 1 / props.theme.aspectRatio * 100}%;
+  height: ${(props) => (1 / props.theme.aspectRatio) * 100}%;
 `;
 
-function Presentation({ children }) {
+const PresentationContentContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: ${(props) => props.contentDirection};
+  justify-content: ${(props) =>
+    props.contentDirection === "row"
+      ? props.verticalAlignment
+      : props.horizontalAlignment};
+  align-items: ${(props) =>
+    props.contentDirection === "row"
+      ? props.horizontalAlignment
+      : props.verticalAlignment};
+`;
+
+function Presentation({
+  children,
+  alignments = "center-center",
+  contentDirection = "row",
+}) {
   const theme = usePresentationTheme();
+  const [verticalAlignment, horizontalAlignment] = alignments.split("-");
 
   return (
     <PresentationWrapper {...{ theme }}>
       <PresentationContainer {...{ theme }}>
-        <div>{children}</div>
+        <PresentationContentContainer
+          {...{ verticalAlignment, horizontalAlignment, contentDirection }}
+        >
+          {children}
+        </PresentationContentContainer>
       </PresentationContainer>
     </PresentationWrapper>
   );
