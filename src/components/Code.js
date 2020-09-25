71944dc149e4f925e4272f03176fe944999a1f4f
ksotis <ks.srikittichai@gmail.com>
2020-09-25 07:01:24 +0700

    add code block syntax highligher

--- src/components/Code.js
+++ src/components/Code.js
@@ -2,9 +2,14 @@ import React from "react";
 import SyntaxHighLighter from "react-syntax-highlighter";
 import { irBlack } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-function Code({ children, language }) {
+function Code({ children, ...codeProps }) {
   return (
-    <SyntaxHighLighter {...{ language }} style={irBlack}>
+    <SyntaxHighLighter
+      {...codeProps}
+      lineNumberStyle={{ color: "rgba(255,255,255,0.5)" }}
+      lineNumberContainerStyle={{ float: 'left', paddingRight: 20 }}
+      style={irBlack}
+    >
       {children}
     </SyntaxHighLighter>
   );
